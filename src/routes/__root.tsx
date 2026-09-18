7dc6ebe7633fe556b9523ee02928c4adb194f7eb
Tejash <tejesh.kamada47@gmail.com>
2026-09-18 13:47:17 +0530

    chore: remove redundant loose assets and update custom KT developer favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -105,7 +105,8 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/favicon.svg", type: "image/svg+xml" },
+      { rel: "alternate icon", href: "/favicon.ico", type: "image/x-icon" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
